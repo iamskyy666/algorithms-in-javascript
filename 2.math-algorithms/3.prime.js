18bf1725eec5be2cc3b52f05a64aa32a45aa6ea7
Soumadip <39thdecember@gmail.com>
2026-03-21 03:31:22 +0530

    isPrime

---
+++ 2.math-algorithms/3.prime.js
@@ -0,0 +1,21 @@
+// isPrime(5)= true (1*5) or (5*1) // Only divided by 1 or the num itself
+// isPrime(4) = false (4,2,1) etc.
+
+function isPrime(num) {
+  if (typeof num === "number" && !isNaN(num)) {
+    if (num < 2) {
+      return false;
+    }
+    for (let i = 2; i < num; i++) {
+      if (num % i === 0) {
+        return false;
+      }
+      return true;
+    }
+  } else {
+    return `Please enter a valid number!`;
+  }
+}
+
+console.log(isPrime(4));
+console.log(isPrime(7));
